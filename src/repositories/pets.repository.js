370a387720e5c7a7922872553e6f11142a1558d2
Caio lima <Caioonepiece70@gmail.com>
2023-05-24 02:56:26 -0300

    repository

--- src/repositories/pets.repository.js
+++ src/repositories/pets.repository.js
@@ -7,40 +7,40 @@ const __filename = fileURLToPath(import.meta.url);
 
 const dbPath = path.resolve(__filename ,'..', 'petShop_db')
 
-class PetsRepository {
-    constructor() {
-      this.db = new sqlite3.Database(dbPath)
-    }
+class PetRepository {
+  constructor() {
+    this.db = new sqlite3.Database(dbPath);
+  }
 
-async create({ proprietarioId , nome , idade , peso , raca}){
+  async create({ nome, idade, peso, raca, proprietarioId }) {
     return new Promise((resolve , reject) => {
-    
-        const id = randomUUID();
+  
+      const id = randomUUID();
 
-        this.db.run('INSERT INTO pets VALUES(? ,? , ?, ?, ? , ?)'), [id, proprietarioId , nome , idade , peso , raca], (err) => {
-            if(err){
-                reject(err)
-            }else{
-                resolve(id)
-            }
+      this.db.run('INSERT INTO pets VALUES(?, ?, ?, ?, ?, ?)', [id, nome, idade, peso, raca, proprietarioId], (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(id);
         }
+      });
+  
     })
-}   
+  }
 
-async list(){
-  return new Promise((resolve , reject) => {
-    this.db.all('SELECT * FROM  pets' , (err , rows) => {
-        if(err){
-            reject(err)
-        }else{
-            resolve(rows)
+  async list() {
+    return new Promise((resolve, reject) => {
+      this.db.all('SELECT * FROM pets', (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
         }
-    })
-  })
-}
+      });
+    });
+  }
 
-
-async listById({ petId }) {
+  async listById({ petId }) {
     return new Promise((resolve, reject) => {
       this.db.get('SELECT * FROM pets WHERE id = ?', petId, (err, row) => {
         if (err) {
@@ -53,8 +53,30 @@ async listById({ petId }) {
       });
     });
   };
+
+  async update({ nome, idade, peso, raca, petId }) {
+    return new Promise((resolve, reject) => {
+      this.db.run('UPDATE pets SET nome = ?, idade = ?, peso = ?, raca = ? WHERE "id" = ?', [nome, idade, peso, raca, petId], (err, row) => {
+        if (err) {
+          reject(err)
+        }  else {
+          resolve(row)
+        }
+      })
+    })
+  }
+
+  async proprietarioId({ proprietarioId }) {
+    return new Promise((resolve, reject) => {
+      this.db.get('SELECT Id FROM proprietarios WHERE id = ?', proprietarioId, (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
 }
 
-
-
-export default new PetsRepository() 
+export default new PetRepository()
