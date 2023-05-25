328e457f7c4c905e167fcac54a1d195349487355
Caio lima <Caioonepiece70@gmail.com>
2023-05-25 03:40:41 -0300

    routes

--- src/routes/pets.routes.js
+++ src/routes/pets.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import PetsController from "../controllers/pets.controller.js";
-import { validatePetMiddleware, validatePetMiddlewareUpdate } from "../middlewares/pets.middleware.js";
+import { validatePetMiddleware, validatePetMiddlewareUpdate } from "../middlewares/pet.middleware.js";
 
 const routes = Router();
 
 routes.post('/', validatePetMiddleware, PetsController.create)
 routes.get('/', PetsController.list)
 routes.get('/:id',PetsController.listById)
-routes.put('/:id', validatePetMiddlewareUpdate )
-routes.delete('/:id', )
+routes.put('/:id', validatePetMiddlewareUpdate , PetsController.updateById )
+routes.delete('/:id', PetsController.deleteById )
 
 export { routes as petRoutes };
