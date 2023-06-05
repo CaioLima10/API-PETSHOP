e3a8c2420c7637810a6c472957ea681236fc6007
Caio lima <caiohenrys50@gmail.com>
2023-06-05 15:49:08 -0300

    routes

--- src/routes/proprietarios.routes.js
+++ src/routes/proprietarios.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import ProprietarioController from '../controllers/proprietario.controller.js';
-import { validateProprietarioMiddleware, validateProprietarioMiddlewareUpdate } from '../middlewares/proprietario.middleware.js'
+import { validateOwnerMiddleware, validateOwnerMiddlewareUpdate } from '../middlewares/proprietario.middleware.js'
 
 const routes = Router();
 
-routes.post('/', validateProprietarioMiddleware, ProprietarioController.create);
+routes.post('/', validateOwnerMiddleware, ProprietarioController.create);
 routes.get('/', ProprietarioController.list);
 routes.get('/:id', ProprietarioController.listById);
 routes.get('/:id/pets', ProprietarioController.listPetsProprietario);
-routes.put('/:id',validateProprietarioMiddlewareUpdate, ProprietarioController.updateById)
+routes.put('/:id',validateOwnerMiddlewareUpdate, ProprietarioController.updateById)
 routes.delete('/:id', ProprietarioController.deleteById);
 
 
